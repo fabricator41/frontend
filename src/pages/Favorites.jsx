60c23596f55555fe244b5270f7ca4747a70d8de5
Emre İlhan <emrilhan@gmail.com>
2024-11-28 19:13:23 +0300

    video tiime 01:00:53

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -4,7 +4,7 @@ function Favorites() {
   return (
     <div className="favorites-empty">
       <h2>No Favorite Movies Yet</h2>
-      <p>Start adding, your movies appear here!</p>
+      <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
   );
 }
